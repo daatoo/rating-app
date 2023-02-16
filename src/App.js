23612436059c9c3806aa533670235e525bd7d2a9
davit cheishvili <davit.cheishvili.06@gmail.com>
2023-02-16 18:37:24 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className=
+        'app absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] flex '>
+
+          
+      </div>
+    );
+  }
+}
+
+export default App;
